116853da1fc2ef3fa8174e162aa20de319d29837
ChrisY03 <46610490+ChrisY03@users.noreply.github.com>
2026-09-14 01:18:17 +0100

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_hash = {}
+        t_hash = {}
+        for char in s:
+            if char in s_hash:
+                s_hash[char] = s_hash[char] + 1
+            else:
+                s_hash[char] = 1
+            
+        for char in t:
+            if char in t_hash:
+                t_hash[char] = t_hash[char] + 1
+            else:
+                t_hash[char] = 1
+        print(t_hash)
+        print(s_hash)
+        if t_hash == s_hash:
+            return True
+        return False
